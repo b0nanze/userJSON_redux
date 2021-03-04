f87a149093355baa1a40500b5e0bbc37d238937a
b0nanze <iumkhaev@gmail.com>
2021-03-04 18:29:02 +0300

    preload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {addTodo} from "./actions";
+import {useEffect} from "react";
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  const todos = useSelector(state => state.todos)
+  const loading = useSelector(state => state.loading)
+
+  useEffect(() => {
+    dispatch(addTodo());
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="h1">Tasks:</h1>
+      {loading ? (
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Загрузка...</span>
+          </div>
+      ) : (
+          todos.map((todo) => {
+              return(
+                  <div>
+                    <ul className="list-group">
+                      <li className="list-group-item">{todo.title}</li>
+                    </ul>
+                  </div>
+              )
+            })
+      )}
+      {/*{todos.map((todo) => {*/}
+      {/*  return(*/}
+      {/*      <div>*/}
+      {/*        {todo.title}*/}
+      {/*      </div>*/}
+      {/*  )*/}
+      {/*/!*})}*!/ если бы данные уже были в объекте, то надо было сделать так*/}
     </div>
   );
 }
---
+++ src/actions.js
@@ -0,0 +1,15 @@
+export const addTodo = () => {
+      return function (dispatch){
+        dispatch({
+          type: 'preload'
+        })
+          fetch('https://jsonplaceholder.typicode.com/todos')
+              .then(response => response.json())
+              .then(json => {
+                  dispatch({
+                      type: 'load',
+                      payload: json
+                  })
+              })
+      }
+}
